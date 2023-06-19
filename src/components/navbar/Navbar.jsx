9c9753864be8dea1820d0f4ba55719650625db69
Hiruka Chansilu <hirukachansilu@gmail.com>
2023-06-19 23:45:51 +0530

    Navbar Animations

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { NavbarContext } from "../../App";
 
 import "./styles.css";
@@ -8,8 +8,40 @@ import logo from "./logo.svg";
 export default function Navbar() {
   const { theme } = useContext(NavbarContext);
 
+  const [hide, setHide] = useState(false);
+
+  useEffect(() => {
+    let prevPos = window.scrollY;
+
+    document.addEventListener("scroll", () => {
+      let currentPos = window.scrollY;
+
+      if (currentPos > 0) {
+        document.getElementById("nav").style.background = "var(--bg-color)";
+        document.getElementById("nav").style.backdropFilter = "blur(5px)";
+      } else {
+        document.getElementById("nav").style.background =
+          "var(--bg-color-trans)";
+        document.getElementById("nav").style.backdropFilter = "blur(0px)";
+      }
+
+      if (prevPos > currentPos) {
+        setHide(false);
+      } else {
+        setHide(true);
+      }
+      prevPos = currentPos;
+    });
+  }, []);
+
   return (
-    <nav className={theme !== "dark" ? "nav-light" : ""}>
+    <nav
+      className={theme !== "dark" ? "nav-light" : ""}
+      id="nav"
+      style={{
+        top: hide ? "-3.5rem" : "0rem",
+      }}
+    >
       <div className="logo-c">
         <img src={logo} />
         Birb
