6873c3377247ad583170ea1e9cb2c159f87da8b6
Hiruka Chansilu <hirukachansilu@gmail.com>
2023-06-19 12:39:18 +0530

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Section4 from "./sections/section4/Section4";
 import Section5 from "./sections/section5/Section5";
 import Section6 from "./sections/section6/Section6";
 import Section7 from "./sections/section7/Section7";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
@@ -16,7 +17,10 @@ function App() {
       <Section4 />
       <Section5 />
       <Section6 />
-      <Section7 />
+      <div className="footer-cropper">
+        <Section7 />
+        <Footer />
+      </div>
     </>
   );
 }
